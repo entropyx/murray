5dab902032a76da6f994d935a2fdd609367f6a0f
IsaacMtz19 <isaac@entropy.tech>
2025-02-03 17:27:01 -0600

    Remove an unncessary parameter of market_correlations function

--- Murray/auxiliary.py
+++ Murray/auxiliary.py
@@ -15,22 +15,21 @@ def cleaned_data(data, col_target, col_locations, col_dates, fill_value=0):
         pd.DataFrame: A cleaned and processed DataFrame, indexed by date and location, with missing
                     values in the target column filled and ready for visualization or analysis.
     """
-    # Remove rows with invalid location values
     invalid_values = ['(not set)']
     data = data[~data[col_locations].isin(invalid_values)]
     data = data.dropna(subset=[col_locations])
 
-    # Clean the location column
     data[col_locations] = data[col_locations].str.strip().str.lower()
+    
 
-    # Rename columns
+    
     data_input = data.rename(columns={
         col_locations: 'location',
         col_target: 'Y',
         col_dates: 'time'
     })
 
-    # Generate a complete date range from the minimum to maximum date
+    
     all_dates = pd.date_range(start=data_input['time'].min(), end=data_input['time'].max(), freq='D')
     data_input['time'] = pd.to_datetime(data_input['time'])
     all_locations = data_input['location'].unique()
@@ -40,17 +39,17 @@ def cleaned_data(data, col_target, col_locations, col_dates, fill_value=0):
     merged_data = pd.merge(full_data, data_input, on=['time', 'location'], how='left')
     merged_data['Y'] = merged_data['Y'].fillna(fill_value)
 
-    # Sort the DataFrame by time and location
+    
     merged_data = merged_data.sort_values(by=['time', 'location'])
 
-    # Identify rows with missing location values
+    
     if merged_data['location'].isna().any():
         raise ValueError("NaN values found in the 'location' column. Please review the data.")
 
     return merged_data
 
 
-def market_correlations(data, excluded_states):
+def market_correlations(data):
     """
     Determines similarity between states using correlations, while excluding specific states.
 
@@ -69,24 +68,24 @@ def market_correlations(data, excluded_states):
     pivoted_data = data.pivot(index='time', columns='location', values='Y')
     correlation_matrix = pivoted_data.corr(method='pearson')
 
-    # Reshape the correlation matrix to long format
+    
     correlation_df = correlation_matrix.reset_index().melt(
         id_vars='location',
         var_name='var2',
         value_name='correlation'
     )
 
-    # Sort values for each state by correlation (highest to lowest)
+    
     sorted_correlation_df = (
         correlation_df
         .sort_values(by=['location', 'correlation'], ascending=[True, False])
         .query("location != var2")
     )
 
-    # Assign a rank based on correlation
+    
     sorted_correlation_df['rank'] = sorted_correlation_df.groupby('location').cumcount() + 2
     
-    # Convert to wide format
+    
     wide_correlation_df = (
         sorted_correlation_df
         .pivot(index='location', columns='rank', values='var2')
