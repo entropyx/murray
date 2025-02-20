d031f007280001e66ed5cf9608aedeadecfecdf4
IsaacMtz19 <isaac@entropy.tech>
2025-02-19 21:28:44 -0600

    Reset the default parameter n_combinations

--- Murray/main.py
+++ Murray/main.py
@@ -52,8 +52,8 @@ def select_treatments(similarity_matrix, treatment_size, excluded_locations):
     max_combinations = comb(n, r)
 
     n_combinations = max_combinations
-    if n_combinations > 200:
-        n_combinations = 200
+    if n_combinations > 5000:
+        n_combinations = 5000
 
 
     combinations = set()
