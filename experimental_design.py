afb4a7dcde6a77893eef5a84028faf84682271b2
IsaacMtz19 <isaac@entropy.tech>
2025-03-09 20:53:22 -0600

    Remove some comments

--- experimental_design.py
+++ experimental_design.py
@@ -324,7 +324,6 @@ def generate_pdf(treatment_group, control_group, holdout_percentage, impact_grap
         
         pdf.set_xy(x_start, y_start + max_header_height)
 
-        # 3) Imprimir datos (3 columnas)
         pdf.set_text_color(*text_color)
         pdf.set_font("Poppins", "", 10)
         y_data_start = pdf.get_y()
@@ -340,7 +339,7 @@ def generate_pdf(treatment_group, control_group, holdout_percentage, impact_grap
         y_data_end = pdf.get_y()
         altura_total = y_data_end - y_data_start
 
-        # 4) Fusionar la 4ta columna
+        
         x_fourth_col = x_start + col_widths[0] + col_widths[1] + col_widths[2]
         pdf.set_xy(x_fourth_col, y_data_start)
 
